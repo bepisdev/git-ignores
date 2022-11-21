7ee6a298be816c1c497990fdf334fa47408ce8cc
Josh Burns <joshyburnss@gmail.com>
2022-11-21 23:58:43 +1100

    generate url from template flag

--- git_ignores/console.py
+++ git_ignores/console.py
@@ -1,2 +1,7 @@
-def run():
-    print("hi")
+import click
+
+@click.command()
+@click.option('--template', '-t', type=str, help='name of the gitignore template to use')
+def run(template):
+    """Generates a .gitignore template from githubs repo"""
+    url_template = f'https://raw.githubusercontent.com/github/gitignore/main/{template}.gitignore'
