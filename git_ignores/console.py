3b4d2fb15fbbbb8a197f41b954f35c9b7dc17983
Josh Burns <joshyburnss@gmail.com>
2022-11-22 00:10:46 +1100

    setup cli that grabs the gitignore template

--- git_ignores/console.py
+++ git_ignores/console.py
@@ -1,7 +1,13 @@
 import click
+import json
+import requests
 
 @click.command()
 @click.option('--template', '-t', type=str, help='name of the gitignore template to use')
 def run(template):
     """Generates a .gitignore template from githubs repo"""
     url_template = f'https://raw.githubusercontent.com/github/gitignore/main/{template}.gitignore'
+    req = requests.get(url_template)
+    if req.status_code == 200:
+        data = req.text
+        print(data)
