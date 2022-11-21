c06a4625945513050dd40b78b5fabe8954edca43
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-11-21 22:21:35 +0000

    add --force flag

--- git_ignores/console.py
+++ git_ignores/console.py
@@ -11,11 +11,21 @@ def _write_to_file(filename, data):
 
 @click.command()
 @click.option('--template', '-t', type=str, help='name of the gitignore template to use')
-def run(template):
+@click.option('--force', '-f', default=False, help='replace existing gitignore file if one exists')
+def run(template, force):
     """Generates a .gitignore template from githubs repo"""
     url_template = f'https://raw.githubusercontent.com/github/gitignore/main/{template}.gitignore'
     req = requests.get(url_template)
     if req.status_code == 200:
         data = req.text
+        fileexists = False
+        
         if Path('./.gitignore').is_file():
-            pass
+            fileexists = True
+        
+        if fileexists is True:
+            if force is False:
+                print(".gitignore already exists, goodbye!")
+                return
+        
+        _write_to_file('./.gitignore', data)
