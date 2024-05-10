06e21c966f9bbb2cba53cd46bc214af5efbbaa1b
Josh Burns <joshyburnss@gmail.com>
2024-05-10 20:03:14 +1000

    better error handling, move HTTP GET into its own function

--- cmd/git-ignores/main.go
+++ cmd/git-ignores/main.go
@@ -33,52 +33,57 @@ func writeIgnoreFile(cont string) error {
 
 }
 
+func fetchGitignoreTemplate(templateName string) (string,error) {
+	templateURL := fmt.Sprintf("https://raw.githubusercontent.com/github/gitignore/main/%s.gitignore", templateNameFlag)
+	res,err := http.Get(templateURL)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("HTTP request failed with status code %d", res.StatusCode)
+	}
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(bodyBytes), nil
+}
+
 func main() {
 	// Parse cli flag options
 	flag.Parse()
 
 	// URL for .gitignore file
 	if templateNameFlag == "" {
-		fmt.Printf("ERROR: Please provide a template via --template\n")
+		fmt.Fprintf(os.Stderr, "ERROR: Please provide a template via --template\n")
+		flag.Usage()
 		os.Exit(1)
 	}
-	templateURL := fmt.Sprintf("https://raw.githubusercontent.com/github/gitignore/main/%s.gitignore", templateNameFlag)
 
-	// Make GET request to templateURL
-	res, err := http.Get(templateURL)
+	gitignoreContents, err := fetchGitignoreTemplate(templateNameFlag)
 	if err != nil {
-		fmt.Printf("ERROR: %s\n", err)
-		os.Exit(1)
-	}
-	if res.StatusCode == http.StatusOK {
-		bodyBytes, err := io.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Create string from GET result
-		gitignoreContents = string(bodyBytes)
-
-	} else {
-		fmt.Println("ERROR: HTTP Request failure")
+		fmt.Fprintf(os.Stderr, "ERROR: Failed to fetch template: %v\n", templateNameFlag)
 		os.Exit(1)
 	}
 
 	// Check if .gitignore exists
 	if _,err := os.Stat(".gitignore"); err == nil {
 		if forceFlag {
-			os.Remove(".gitignore")
+			if err := os.Remove(".gitignore"); err != nil {
+				fmt.Fprintf(os.Stderr, "ERROR: Failed to remove existing .gitignore file\n")
+				os.Exit(1)
+			}
 		} else {
-			fmt.Println("ERROR: .gitignore already exists, use --force to overwrite")
+			fmt.Fprintf(os.Stderr, "ERROR: .gitignore already exists. Use --force to overwrite\n")
+			flag.Usage()
 			os.Exit(1)
 		}
 	} 
 
 	// Write contents to .gitignore
-	err = writeIgnoreFile(gitignoreContents)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if err := writeIgnoreFile(gitignoreContents); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: Failed to write .gitignore: %v\n", err)
+			os.Exit(1)
 	}
 }
